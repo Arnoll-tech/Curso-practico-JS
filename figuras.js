c25969a3687fc8623e68c4229dfd9f46316a21e3
arnoll-tech <arnolbermudez28@gmail.com>
2022-05-12 20:19:33 -0500

    se agrego el lector de id al codigo para guardar el valor y retornarlo en una alerta

--- figuras.js
+++ figuras.js
@@ -72,3 +72,20 @@ function areaCirculo(radio) {
 //console.log("El area del circulo es: " + areaCirculo() + " cm*2");
 
 console.groupEnd();
+
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
