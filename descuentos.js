56f21ce93203b4978dffab0cee3b53e56c29401d
arnoll-tech <arnolbermudez28@gmail.com>
2022-05-18 20:18:29 -0500

    se termino de crear la calculadora de descuentos

--- descuentos.js
+++ descuentos.js
@@ -9,8 +9,24 @@ console.log({
      precioConDescuento,
  });*/
 
- function calcularPrecioCOnDescuento(precio, descuento) {
+ function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
     return precioConDescuento;
  }
+
+ function onclickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
+
+     const inputDiscount = document.getElementById("inputDiscount");
+     const discountValue = inputDiscount.value;
+
+     const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+     const resultP = document.getElementById("resultP");
+     resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+ }
+
+ 
+     
