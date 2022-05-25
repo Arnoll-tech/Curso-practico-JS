3a714459ea3ca5706b1e10aa91bd19fff3a44230
arnoll-tech <arnolbermudez28@gmail.com>
2022-05-24 20:13:02 -0500

    se hizo un array y un swith para los descuentos

--- descuentos.js
+++ descuentos.js
@@ -9,6 +9,12 @@ console.log({
      precioConDescuento,
  });*/
 
+ const coupons = [
+    "JuanDC_es_Batman",
+    "pero_no_le_digas_a_nadie",
+    "es_un_secreto",
+ ];
+
  function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
@@ -19,14 +25,29 @@ console.log({
     const inputPrice = document.getElementById("inputPrice");
     const priceValue = inputPrice.value;
 
-     const inputDiscount = document.getElementById("inputDiscount");
-     const discountValue = inputDiscount.value;
+     const inputCoupon = document.getElementById("inputCoupon");
+     const couponValue = inputCoupon.value;
 
-     const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+     let descuento;
+
+     switch (couponValue) {
+         case coupons[0]://"JuanDC_es_Batman"
+           descuento = 15;
+            break;
+         case coupons[1]://"pero_no_le_digas_a_nadie"
+            descuento = 30;
+            break;
+         case coupons[2]://"es_un_secreto"
+            descuento = 25;
+            break;
+     
+        default:
+           break;
+     }
+
+     const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
 
      const resultP = document.getElementById("resultP");
      resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
- }
-
- 
      
+ }
