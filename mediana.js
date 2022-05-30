ddc3a61929bdda83e6ca5ef51fba1316ff9b0346
arnoll-tech <arnolbermudez28@gmail.com>
2022-05-30 04:16:40 -0500

    sehizo una funcion esPar para saber siel array es par o no

---
+++ mediana.js
@@ -0,0 +1,27 @@
+const lista1 = [
+    100,
+    200, 
+    500, 
+    600,
+    3000, 
+    
+];
+
+const mitadLista1 = parseInt(lista1.length / 2);
+function esPar(numerito) {
+    if (numerito % 2 === 0) {
+        
+    } else {
+        
+    }
+    return false
+}
+
+let mediana = 0;
+
+if (lista1 [lista1.length % 2 === 0]) {
+    
+} else {
+    mediana = lista1[mitadLista1];
+    
+}
