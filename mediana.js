67677dca2d1572c5f3806b8f48476515206ac0c5
arnoll-tech <arnolbermudez28@gmail.com>
2022-06-16 17:44:14 -0500

    se agrego una funcion y se termtino de modificar para cuando el array es par

--- mediana.js
+++ mediana.js
@@ -1,8 +1,25 @@
+function calcularMediaAritmetica(lista) {
+ 
+    /*let sumaLista = 0;
+
+    for (let i = 0; i < lista.length; i++) {
+    sumaLista = sumaLista + lista[i];
+    }*/
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+
+}
+
 const lista1 = [
     100,
-    200, 
-    500, 
-    600,
+    300, 
+    300,
     3000, 
     
 ];
@@ -10,7 +27,8 @@ const lista1 = [
 const mitadLista1 = parseInt(lista1.length / 2);
 function esPar(numerito) {
     if (numerito % 2 === 0) {
-        
+ 
+
     } else {
         
     }
@@ -19,9 +37,19 @@ function esPar(numerito) {
 
 let mediana = 0;
 
-if (lista1 [lista1.length % 2 === 0]) {
-    
+if (esPar (lista1.length)) {
+    const elemento1 = lista1[mitadLista1 - 1];
+    const elemento2 = lista1[mitadLista1];
+
+    const promedioElemento1y2 = calcularMediaAritmetica([
+        elemento1,
+        elemento2,
+    ]);
+
+    mediana = promedioElemento1y2;
+
 } else {
     mediana = lista1[mitadLista1];
     
 }
+
